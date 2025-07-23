210adb02155131e2510355c64641a2fac007416c
erick moses <moseserickomondi@gmail.com>
2025-07-23 18:34:51 +0300

    added the user-account-page,made some changes on the NavBar 'fiilRule and clipRule' i updated the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import CreateEventForm from "./components/CreateEventForm"
 import NavBar from "./components/Navbar"
+import UserAccountPage from "./components/user-account-page"
+
 
 function App() {
   return (
-    <>
+    <Router>
       <NavBar />
       <div className="py-10">
-        <CreateEventForm />
+        <Routes>
+        <Route path="/" element={<CreateEventForm/>} />
+        <Route path="/create-event" element={<CreateEventForm />} />
+        <Route path="/profile" element={<UserAccountPage />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   )
 }
 
-export default App
+export default App;
