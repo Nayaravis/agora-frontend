4841a149345e148746cebb3bc0ec6fb2d2cb4b45
Navis Ayara <navis.ochieng@student.moringaschool.com>
2025-07-24 05:30:10 +0300

    adds random color generation for event image placeholders

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,15 +1,15 @@
 import { motion } from "motion/react";
 import { Link } from "react-router";
 
-function EventCard({ id, title, location, date, notes, createdBy }) {
+function EventCard({ id, title, location, date, notes, createdBy, cardColor }) {
     return (
         <Link to={`/events/${id}`}>
             <div className="w-72 cursor-pointer">
                 <motion.div 
-                    className="bg-red-500 w-full h-44 rounded-2xl"
+                    className={`${cardColor} w-full h-44 rounded-2xl`}
                     whileHover={{scale: 1.1}}
                 >
-                
+
                 </motion.div>
                 <div className="py-3 px-3.5">
                     <p className="font-bold text-lg">{title}</p>
--- src/components/EventListPage.jsx
+++ src/components/EventListPage.jsx
@@ -3,6 +3,29 @@ import EventCard from "./EventCard";
 
 function EventsList() {
     const [events, updateEvents] = useState([]);
+    const vibrantTailwindColors = [
+      "bg-red-500",
+      "bg-pink-500",
+      "bg-fuchsia-500",
+      "bg-purple-500",
+      "bg-violet-500",
+      "bg-indigo-500",
+      "bg-blue-500",
+      "bg-sky-500",
+      "bg-cyan-500",
+      "bg-teal-500",
+      "bg-emerald-500",
+      "bg-green-500",
+      "bg-lime-500",
+      "bg-yellow-400",
+      "bg-amber-400",
+      "bg-orange-500"
+    ];
+
+    function generateColor() {
+      const index = Math.floor(Math.random() * vibrantTailwindColors.length);
+      return vibrantTailwindColors[index];
+    }
     
     useEffect(() => {
         fetch("https://agora-backend-pg31.onrender.com/api/events")
@@ -18,9 +41,9 @@ function EventsList() {
             <div className="text-gray-500 text-lg">
                 <span>Discover exciting events happening near you and around the globe</span>
             </div>
-            <div className="py-6">
+            <div className="py-6 flex gap-6">
                 {events.map(event => {
-                    return <EventCard key={event.id} title={event.title} location={event.location} date={event.datetime} id={event.id}/>
+                    return <EventCard key={event.id} title={event.title} location={event.location} date={event.datetime} id={event.id} cardColor={generateColor()}/>
                 })}
             </div>
         </div>
